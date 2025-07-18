71fa2fc34e574c8e6071d307844baaa56c910240
liwenjun-code <wenjun_li0512@163.com>
2025-07-18 12:48:02 +0800

    python 的基本语法练习

---
+++ hm_02_text.py
@@ -0,0 +1,69 @@
+"""
+name = '张三'
+age = 18
+print(name,age)
+"""
+from sys import flags
+
+# shift+回车——换行
+
+"""
+# 整型
+num = 10
+print(num,type(num))  # 10 <class 'int'>
+
+# 浮点型
+num2 = 3.14
+print(num2,type(num2)) # 3.14 <class 'float'>
+# 布尔型
+num3 = True
+print(num3,type(num3)) # True <class 'bool'>
+#字符串类型
+name = "lisi"
+print(name,type(name))
+
+"""
+
+"""
+age = input("请输入你的名字：")
+print(age)
+
+num = input("请输入一个数字：")
+print(num)
+
+"""
+
+"""
+age = input("please input your age:")
+Int_age = int(age)
+print(age,type(age))
+print(Int_age,type(Int_age))
+
+num = input("please number:")
+Num_1 = float(num)
+print(num,type(num))
+
+name = input("please input your name:")
+Name_1 = str(name)
+print(name,type(name))
+print(Name_1,type(Name_1))
+
+"""
+
+"""
+name = '张三'
+age = 18
+height = 1.98
+
+# 按照如下的格式显示个人信息
+# 我的名字是xx，年龄是xx岁，身高xxm
+print('我的名字是 {},年龄是 {} 岁, 身高 {} m'.format(name,age,height))
+print(f'我的名字是{name},年龄是{age}岁，身高{height}m')
+
+num1 = float(input("please inpur num1:"))
+num2 = float(input("please input num2:"))
+sum = num1 + num2
+print(f'{num1}+{num2} = {sum}')
+
+"""
+
